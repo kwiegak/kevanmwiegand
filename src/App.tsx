f6e83b35b5109dc1002ad8f6d45526bc522a482a
kwiegak <rduke691@gmail.com>
2024-03-02 12:19:03 -0500

    Add-home-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from './components/home/HomePage';
 
 function App() {
   return (
       <div className="App">
-          This is a test
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
     </div>
   );
 }
