5dad657f8a90c2363a6f9a62fd075470abda14ee
kwiegak <rduke691@gmail.com>
2024-03-02 11:51:47 -0500

    Update-App-Level

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+      <div className="App">
+          This is a test
+    </div>
+  );
+}
+
+export default App;
