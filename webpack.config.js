c0d11a64e5baf812d19937f9d56e62de2f9bde65
kwiegak <rduke691@gmail.com>
2024-03-14 09:43:44 -0400

    Fix-Web-Pack-Issue-1

--- webpack.config.js
+++ webpack.config.js
@@ -48,10 +48,6 @@ const config = {
   },
     resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-  },
-      output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   }
 };
 
