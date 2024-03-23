6377f5c09523a1ef27ec006f7991704e144fd79b
kwiegak <rduke691@gmail.com>
2024-03-23 07:10:17 -0400

    Add graphql test

--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -1,15 +1,28 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './Photos.module.css';
 import PhotoAlbum from "react-photo-album";
 import { Amplify } from 'aws-amplify';
 import API from '@aws-amplify/api';
 import Storage from '@aws-amplify/storage'
+import { listTodos } from "../../graphql/queries";
+import {
+  createTodo as createNoteMutation,
+  deleteTodo as deleteNoteMutation,
+} from "../../graphql/mutations";
 
 const photos = [
   { src: "./images/11.jpg", width: 1600, height: 900 },
   { src: "./images/22.jpg", width: 1600, height: 900 },
   { src: "./images/44.jpg", width: 1600, height: 900 },
 ];
+
+useEffect(() => {  
+    fetchTodos();
+}, []);
+
+async function fetchTodos() {
+  console.log("Test");
+  }
 
 interface PhotosProps {}
 
