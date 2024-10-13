f39c76fd071ceab2a7ea68039595b9447ca8b484
kwiegak <rduke691@gmail.com>
2024-10-13 07:25:18 -0400

    shuffle-photo-list

--- src/components/common/CustomStorageImage.tsx
+++ src/components/common/CustomStorageImage.tsx
@@ -6,12 +6,32 @@ import { StorageImage, StorageManager } from '@aws-amplify/ui-react-storage';
 async function getAllImageFilePathsFromS3Bucket(path: string) {
     let filepaths: Array<string> = [];
     const result = await list({ path: 'public/' + path });
-    let listOfS3Items = result.items;
+    let listOfS3Items = shuffle(result.items);
     listOfS3Items.forEach((x) => {
-        if (x.size)
-            filepaths.push(x.path);
+        if (x.length > 0) {
+            filepaths.push(x);
+        }
     })
     return filepaths
+}
+
+function shuffle(array: Array<any>) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    var randomizedArray = array.map(function (array) {
+        return array['path'];
+    });
+
+    return randomizedArray;
 }
 
 interface CustomStorageProps { path: string }
