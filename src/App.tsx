ede00fa8f04921390be8421ea9e5ae71f13940f3
kwiegak <rduke691@gmail.com>
2024-10-13 14:23:21 -0400

    add-dropdown-add-germany

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import NewYork from './components/newyork/NewYork';
 import RedHot from './components/redhotchilipeppers/RedHot';
 import config from './amplifyconfiguration.json';
 import './App.css';
+import HomePage from './components/home/HomePage';
+import Germany from './components/germany/Germany';
 
 Amplify.configure(config);
 
@@ -16,11 +18,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <Header/>
+        <Header />
         <Routes>
-           <Route path='*' element= {<NewYork/>} />
-           <Route path='/rhcp' element= {<RedHot/>} />
-           <Route path="/dallas" element={<Dallas />} />
+          <Route path='/kevanwiegand' element={<HomePage />} />
+          <Route path='/*' element={<NewYork />} />
+          <Route path='/nyc' element={<NewYork />} />
+          <Route path='/germany' element={<Germany />} />
+          <Route path='/rhcp' element={<RedHot />} />
+          <Route path="/dallas" element={<Dallas />} />
         </Routes>
       </div>
     );
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react';
-import 'bootstrap/dist/css/bootstrap.css'; 
+import 'bootstrap/dist/css/bootstrap.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from './Header.module.css';
 
-interface HeaderProps {}
+interface HeaderProps { }
 
 const Header: FC<HeaderProps> = () => (
   <Navbar bg="dark" data-bs-theme="dark" fixed='top'>
-  <Nav className="me-auto">
-          <Nav.Link href="/">New York City</Nav.Link>
-          <Nav.Link href="/rhcp">Red Hot Chili Peppers</Nav.Link>
-          <Nav.Link href="/dallas">Dallas</Nav.Link>
-        </Nav>
-    </Navbar>
+    <Nav className="me-auto">
+      <Nav.Link href="/kevanwiegand">Kevan Wiegand</Nav.Link>
+      <NavDropdown title="Photo Collection" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/dallas">Dallas</NavDropdown.Item>
+        <NavDropdown.Item href="/germany">Germany</NavDropdown.Item>
+        <NavDropdown.Item href="/nyc">New York City</NavDropdown.Item>
+        <NavDropdown.Item href="/rhcp">Red Hot Chili Peppers</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
 );
 
 export default Header;
