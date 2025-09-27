48af9aa4c6bc0707ad60ba7fb74abda9a12b2718
kwiegak <rduke691@gmail.com>
2025-09-27 10:10:10 -0500

    testing-auth

--- src/components/newyork/NewYork.tsx
+++ src/components/newyork/NewYork.tsx
@@ -4,7 +4,7 @@ import CustomStorageImage from "../common/CustomStorageImage";
 const NewYork: FC = () => {
   return (
     <>
-      <h1 style={{ textAlign: "center", margin: "16px 0" }}>New York Gallery</h1>
+      <h1 style={{ textAlign: "center", margin: "16px 0" }}>New York Gallery!</h1>
       <CustomStorageImage path="nyc" />
     </>
   );
