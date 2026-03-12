9d9da3ea5d6aceb7af970f8aeb089f95a3a091aa
kwiegak <rduke691@gmail.com>
2026-03-12 09:17:28 -0500

    Renaming-and-restructure

--- src/config/photoCollections.ts
+++ src/config/photoCollections.ts
@@ -1,4 +1,9 @@
-export const photoCollections = [
+type PhotoCollection = {
+  key: string;
+  label: string;
+};
+
+export const photoCollections: PhotoCollection[] = [
   { key: "dallas", label: "Dallas" },
   { key: "germany", label: "Germany" },
   { key: "nyc", label: "New York City" },
