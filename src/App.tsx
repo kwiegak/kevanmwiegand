8dab93e1a6b668c0b70eda5a7db39af38a7c6a8a
kwiegak <rduke691@gmail.com>
2026-03-12 08:02:31 -0500

    Restore-Staging-Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
-import { Amplify } from 'aws-amplify';
 import Header from './components/common/Header';
-import config from './amplifyconfiguration.json';
 import './App.css';
 import Gallery from './components/gallery/Gallery';
 
-Amplify.configure(config);
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/:category" element={<Gallery />} />
-          <Route path="/" element={<Gallery />} />
-        </Routes>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/:category?" element={<Gallery />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
