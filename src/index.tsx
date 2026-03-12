1c66fc3593f6da31cc072aada8227480a5eb3057
kwiegak <rduke691@gmail.com>
2026-03-12 08:23:21 -0500

    Reformat

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import { Amplify } from 'aws-amplify';
-import config from './amplifyconfiguration.json';
+import { configureAmplify } from "./config/amplify";
 
-Amplify.configure(config);
+configureAmplify();
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
